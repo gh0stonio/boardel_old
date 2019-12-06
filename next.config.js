a84fdb9de971e1bd8d0f8afcd8d161c58bb5b819
gh0stonio <antoine.leveque.pro@gmail.com>
2019-12-06 23:06:20 +0100

    chore: implement PWA (manifest & offline service-worker)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withOffline = require('next-offline')
+
+const nextConfig = {
   target: 'serverless',
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
 }
+
+module.exports = withOffline(nextConfig)
